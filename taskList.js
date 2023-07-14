986e7a0dbff976524d8e445ca2b313e34e394093
Andres castañeda <estiven.casta12@gmail.com>
2023-07-14 08:19:27 -0500

    ruta protegida y login

--- taskList.js
+++ taskList.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 const app = express();
 const listEditRouter = require('./list-edit-router');
 const listViewRouter = require('./list-view-router');
 
 app.use(express.json());
+
+dotenv.config();
 
 const validateMethod = (req, res, next) => {
   const validMethods = ['GET', 'POST', 'PUT', 'DELETE'];
@@ -26,8 +30,54 @@ app.get('/tasks', (req, res) => {
   res.json(tasks);
 });
 
-app.get('/tasks/all', (req, res) => {
-  res.json(tasks);
+app.get('/tasks/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const task = tasks.find((task) => task.id === id);
+  if (task) {
+    res.json(task);
+  } else {
+    res.status(404).json({ error: 'No se encontró la tarea con el ID proporcionado.' });
+  }
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Verificar las credenciales del usuario
+  if (username === 'Andres' && password === '900726') {
+    // Generar un token JWT
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Inicio de sesión exitoso', token });
+  } else if (username === 'Estiv' && password === '123456') {
+    // Generar un token JWT
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Inicio de sesión exitoso', token });
+  } else {
+    res.status(401).json({ error: 'Credenciales inválidas' });
+  }
+});
+
+// Middleware para validar el token JWT
+const validateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    const token = authHeader.split(' ')[1];
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      res.status(401).json({ error: 'Token inválido' });
+    }
+  } else {
+    res.status(401).json({ error: 'Token de autenticación no encontrado' });
+  }
+};
+
+// Ruta protegida
+app.get('/protected', validateToken, (req, res) => {
+  res.json({ message: 'Ruta protegida', user: req.user });
 });
 
 app.listen(4000, () => {
